a6b710f9a9ccd70f17d00811f185e06aec4e1d2b
Anna Zharova <a.zharova.muctr@gmail.com>
2022-10-27 16:49:52 +0300

    need logic for winner

--- src/application.js
+++ src/application.js
@@ -12,23 +12,53 @@ const generateField = () => {
     }
     return tableEl;
   };
-  
-  export default () => {
-    const tableEl = generateField();
-  
-    let currentSymbol = 'x';
-    const switchPlayer = () => {
-      currentSymbol = currentSymbol === 'x' ? 'o' : 'x';
-    };
-  
-    tableEl.addEventListener('click', (e) => {
-      if (e.target.textContent === 's') {
-        e.target.textContent = currentSymbol;
+  const check = () => {
+    let result = '';
+    const arrWin = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  const boxes = document.getElementsByTagName('td');
+    for (const varWin of arrWin) {
+      for (const box of varWin) {
+        if (boxes[arrWin[box]].innerHTML == 'x') {
+        result ='крестики';
+        prepearingResult(result);
+        } else if (boxes[arrWin[box]].innerHTML == 'o') {
+        result ='нолики';
+        prepearingResult(result);
+        }
       }
-      switchPlayer();
-    });
-  
-    const root = document.querySelector('.root');
-    root.append(tableEl);
+    }
+    return result;
   };
   
+  const prepearingResult = (winner) => {
+    const winnerBox = document.querySelector('.winner');
+    winnerBox.textContent = `Победили${winner}`;
+  }
+
+export default () => {
+  const tableEl = generateField();
+  let currentSymbol = 'x';
+  const switchPlayer = () => {
+  currentSymbol = currentSymbol === 'x' ? 'o' : 'x';
+  };
+  
+  tableEl.addEventListener('click', (e) => {
+    if (e.target.textContent === 's') {
+      e.target.textContent = currentSymbol;
+    }
+    switchPlayer();
+  });
+  
+  const root = document.querySelector('.root');
+  root.append(tableEl);
+  check();
+};
